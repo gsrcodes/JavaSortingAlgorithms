c17895112cfe0ff0901a445290435258a09091c7
Guilherme Souza <guilhermesouza281@protonmail.com>
2024-04-09 14:12:09 -0300

    feat: selectionsort, bubblesort, shakesort

--- javasortingalgorithms/src/List.java
+++ javasortingalgorithms/src/List.java
@@ -54,4 +54,78 @@ public class List {
         }
         System.out.println(print);
     }
+
+    public void selectionSort() {
+        Node current = start, compare, posSmaller;
+        int aux;
+        while (current.getNext() != null) {
+            compare = current;
+            posSmaller = current;
+
+            while(compare != null) {
+                if (compare.getInfo() < posSmaller.getInfo()) {
+                    posSmaller = compare;
+                }
+
+                compare = compare.getNext();
+            }
+
+            aux = current.getInfo();
+            current.setInfo(posSmaller.getInfo());
+            posSmaller.setInfo(aux);
+            current = current.getNext();
+        }
+    }
+
+    public void bubbleSort() {
+        Node end = this.end, current;
+        int auxInt;
+        boolean swap = true;
+        while(end != this.start.getNext() && swap) {
+            current = start;
+            swap = false;
+            while(current != end) {
+                if(current.getInfo() > current.getNext().getInfo()) {
+                    swap = true;
+                    auxInt = current.getNext().getInfo();
+                    current.getNext().setInfo(current.getInfo());
+                    current.setInfo(auxInt);
+                }
+                current = current.getNext();
+            }
+            end = end.getPrev();
+        }
+    }
+
+    public void shakeSort() {
+        Node end = this.end, start = this.start, current;
+        int auxInt;
+        boolean swap = true;
+        while(end != start && swap) {
+            current = start;
+            swap = false;
+            while(current != end) {
+                if(current.getInfo() > current.getNext().getInfo()) {
+                    swap = true;
+                    auxInt = current.getNext().getInfo();
+                    current.getNext().setInfo(current.getInfo());
+                    current.setInfo(auxInt);
+                }
+                current = current.getNext();
+            }
+            end = end.getPrev();
+
+            current = end;
+            while(current != start) {
+                if(current.getInfo() < current.getPrev().getInfo()) {
+                    swap = true;
+                    auxInt = current.getPrev().getInfo();
+                    current.getPrev().setInfo(current.getInfo());
+                    current.setInfo(auxInt);
+                }
+                current = current.getPrev();
+            }
+            start = start.getNext();
+        }
+    }
 }
--- javasortingalgorithms/src/Main.java
+++ javasortingalgorithms/src/Main.java
@@ -9,5 +9,7 @@ public class Main {
             list.insertEnd(randomValue);
         }
         list.display();
+        list.bubbleSort();
+        list.display();
     }
 }

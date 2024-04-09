3bf27ba82f1bb74b35882c8383440451deb8050d
Guilherme Souza <guilhermesouza281@protonmail.com>
2024-04-09 14:10:02 -0300

    feat: list and node classes

---
+++ javasortingalgorithms/src/List.java
@@ -0,0 +1,57 @@
+public class List {
+    Node start;
+    Node end;
+
+    public List() {
+        this.start = null;
+        this.end = null;
+    }
+
+    public Node getStart() {
+        return start;
+    }
+
+    public void setStart(Node start) {
+        this.start = start;
+    }
+
+    public Node getEnd() {
+        return end;
+    }
+
+    public void setEnd(Node end) {
+        this.end = end;
+    }
+
+    public void insertStart (int info) {
+        Node newNode = new Node(start, null, info);
+        if(start == null)
+            start = end = newNode;
+        else {
+            start.setPrev(newNode);
+            start = newNode;
+        }
+    }
+
+    public void insertEnd(int info) {
+        Node newNode = new Node(null, end, info);
+        if(end == null)
+            end = start = newNode;
+        else {
+            end.setNext(newNode);
+            end = newNode;
+        }
+    }
+
+    public void display() {
+        Node aux = start;
+        StringBuilder print = new StringBuilder();
+        while(aux != null){
+            print.append(aux.getInfo());
+            aux = aux.getNext();
+            if(aux != null)
+                print.append(" - ");
+        }
+        System.out.println(print);
+    }
+}
---
+++ javasortingalgorithms/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        List list = new List();
+        for (int i = 0; i < 8; i++) {
+            int randomValue = random.nextInt(100);
+            list.insertEnd(randomValue);
+        }
+        list.display();
+    }
+}
